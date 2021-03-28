7894c9e9250d31dc3222cb40853c340c62c3112c
Christian Cabato <ChristianMicar.Cabato@uts.edu.au>
2021-03-28 21:20:23 +1100

    [CC] Removed individual csv and inserted test python script

---
+++ run.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import normalize
+from sklearn.manifold import TSNE
+
+
+stocks = pd.read_csv('company-stock-movements-2010-2015-incl.csv', index_col = 0)
+movements = stocks.values
+companies = stocks.index
+
+normalized_movements = normalize(movements)
+
+model = TSNE(learning_rate = 50)
+
+tsne_features = model.fit_transform(normalized_movements)
+
+xs = tsne_features[:,0]
+ys = tsne_features[:,1]
+
+fig, ax = plt.subplots(figsize = [15, 10])
+plt.scatter(xs, ys, alpha = 0.5)
+
+for x, y, company in zip(xs, ys, companies):
+    plt.annotate(company, (x, y), fontsize=9, alpha=0.75)
+plt.tight_layout
+plt.savefig('test.png') #temporary fix, this saves output as a png file
